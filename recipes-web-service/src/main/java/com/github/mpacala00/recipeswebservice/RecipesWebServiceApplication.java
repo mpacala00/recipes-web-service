38aade076e9f0d1cbb64bb8cb75e1b6e7d903d85
Mikołaj Pacała <mpacala@gabos.pl>
2022-11-18 22:52:18 +0100

    Spring app init

---
+++ recipes-web-service/src/main/java/com/github/mpacala00/recipeswebservice/RecipesWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.mpacala00.recipeswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesWebServiceApplication.class, args);
+	}
+
+}
---
+++ recipes-web-service/src/test/java/com/github/mpacala00/recipeswebservice/RecipesWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mpacala00.recipeswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
