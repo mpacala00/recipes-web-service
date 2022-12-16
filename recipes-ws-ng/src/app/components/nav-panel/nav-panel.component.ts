b4fe8d81e146e3f07e8e872cb843a32d9f7f6554
mpacala00 <mpacala00@gmail.com>
2022-12-16 18:49:24 +0100

    Adding bootstrap

--- recipes-ws-ng/src/app/components/nav-panel/nav-panel.component.ts
+++ recipes-ws-ng/src/app/components/nav-panel/nav-panel.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-panel',
   templateUrl: './nav-panel.component.html',
-  styleUrls: ['./nav-panel.component.sass']
+  styleUrls: ['./nav-panel.component.scss']
 })
 export class NavPanelComponent implements OnInit {
 
