9b51f60a048b037794b343a1646c787ca28990ab
ryba <karolina@inpeak.pl>
2022-12-16 20:58:41 +0100

    Change home-page dir to components folder, add recipe components

--- recipes-ws-ng/src/app/app.module.ts
+++ recipes-ws-ng/src/app/app.module.ts
@@ -4,19 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavPanelComponent } from './components/nav-panel/nav-panel.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { RecipeCardComponent } from './components/home-page/recipe-card/recipe-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavPanelComponent,
-    HomePageComponent
+    HomePageComponent,
+    RecipeCardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
