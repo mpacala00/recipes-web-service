865e997c0135967f10e228deae33b283ca4a8176
pawel0603 <pawel_0603@wp.pl>
2022-12-17 14:22:41 +0100

    Adding UnitOfMeasure

--- recipes-web-service/src/main/java/com/github/mpacala00/recipeswebservice/repository/UnitOfMeasureRepository.java
+++ recipes-web-service/src/main/java/com/github/mpacala00/recipeswebservice/repository/UnitOfMeasureRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.Optional;
 public interface UnitOfMeasureRepository extends MongoRepository<UnitOfMeasure, String>{
 
-    @Query("{name:'?0'}")
+    @Query("{unit:'?0'}")
     Optional<UnitOfMeasure> findByUnit(String unit);
 }
