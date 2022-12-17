c50cef23526a6bc56cdb34c9f627762187cbf82e
ryba <karolina@inpeak.pl>
2022-12-17 13:07:00 +0100

    Create browse recipes component, add routing

--- recipes-ws-ng/src/app/app-routing.module.ts
+++ recipes-ws-ng/src/app/app-routing.module.ts
@@ -13,6 +13,11 @@ const routes: Routes = [
     component: HomePageComponent,
     pathMatch: 'full',
   },
+  {
+    path: 'browse-recipes',
+    component: BrowseRecipesPageComponent,
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
