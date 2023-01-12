9ce7fc183ff45a0580f641d0767e5f357c5b207f
mpacala00 <mpacala00@gmail.com>
2023-01-12 19:49:38 +0100

    Small fixes

--- recipes-ws-ng/src/app/components/recipe-page/recipe-page.component.ts
+++ recipes-ws-ng/src/app/components/recipe-page/recipe-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Recipe } from 'src/app/model/recipe';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-page',
@@ -8,16 +9,22 @@ import { Recipe } from 'src/app/model/recipe';
   styleUrls: ['./recipe-page.component.scss'],
 })
 export class RecipePageComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private recipeService: RecipeService) {}
 
-  @Input()
   recipe: Recipe;
 
   ngOnInit(): void {
-    if (!history.state.recipe) {
-      this.router.navigate(['/']); //return to home page if refreshed
-    }
+    // if (!history.state.recipe) {
+    //   this.router.navigate(['/']); //return to home page if refreshed
+    // }
 
-    this.recipe = history.state.recipe;
+    // this.recipe = history.state.recipe;
+    let recipeId = this.router.url.split('/recipe/')[1];
+    this.recipeService.getRecipeById(recipeId).subscribe({
+      next: (res) => {
+        this.recipe = res;
+      },
+      error: (err) => console.error('error while fetching recipe', err),
+    });
   }
 }
--- recipes-ws-ng/src/app/services/recipe.service.ts
+++ recipes-ws-ng/src/app/services/recipe.service.ts
@@ -16,11 +16,19 @@ export class RecipeService {
   }
 
   postRecipe(recipe: Recipe) {
-    return this.http.post<Recipe>(this.recipesUrl, recipe, {observe: "response"});
+    return this.http.post<Recipe>(this.recipesUrl, recipe, {
+      observe: 'response',
+    });
   }
 
   uploadRecipeImage(recipeId: string, imageFormData: FormData) {
-    console.log('image url', `${this.recipesUrl}/${recipeId}/image`)
-    return this.http.post<FormData>(`${this.recipesUrl}/${recipeId}/image`, imageFormData);
+    return this.http.post<FormData>(
+      `${this.recipesUrl}/${recipeId}/image`,
+      imageFormData
+    );
+  }
+
+  getRecipeById(recipeId: string) {
+    return this.http.get<Recipe>(`${this.recipesUrl}/${recipeId}`);
   }
 }
